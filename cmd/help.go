e345b7eca7e140442d0de2983c5dd76c94230e05
ubuntu <schadachal71@gmail.com>
2025-11-28 10:32:23 +0000

    added clarity  for help commands

--- cmd/help.go
+++ cmd/help.go
@@ -3,13 +3,17 @@ package main
 import "fmt"
 
 func printHelp() {
-	fmt.Println("Splitwise CLI Commands:")
+	fmt.Print("Splitwise Commands guide:\n\n")
+	fmt.Print("splitwise <OPTIONS> <COMMAND> [ARGS...]\n\n")
+	fmt.Println("OPTIONS:")
 	fmt.Println("  help                                  - Show help")
 	fmt.Println("  bill                                  - CRUD operations on Bills")
 	fmt.Println("  participant                           - CRUD operations on participants")
-	fmt.Println("  item                                  - CRUD operations on items")
-	fmt.Println("  add-bill --id <id> --tax <percent>    - Create a new bill")
-	fmt.Println("  add-participant --bill <id> --id <pid> --name <name>")
-	fmt.Println("  add-item --bill <id> --id <iid> --name <name> --price <n> --shared-by p1,p2")
-	fmt.Println("  split --bill <id>                     - Calculate split for bill")
+	fmt.Print("  item                                  - CRUD operations on items\n\n")
+
+	fmt.Println("COMMANDS:")
+	fmt.Println("  add-bill --id <id> --tax <percent>							- Create a new bill")
+	fmt.Println("  add-participant --bill <id> --id <pid> --name <name>					- Add participant to bill")
+	fmt.Println("  add-item --bill <id> --id <iid> --name <name> --price <n> --shared-by p1,p2		- Add item to bill")
+	fmt.Println("  split --bill <id>                     						- Calculate split for bill")
 }
