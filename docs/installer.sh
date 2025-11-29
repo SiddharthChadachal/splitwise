04068bc9e3dbcd1521967deef1d0f340b82b6a91
SiddharthChadachal <SiddharthChadachal@users.noreply.github.com>
2025-11-29 22:24:08 +0000

    deploy: 392eaec9905f8781f2ca9bdfbb377a9cf236d50b

--- docs/installer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-echo "Installing Splitwise Core package..."
-GOOS=$OS GOARCH=$ARCH go build -o splitwise ./cmd/
-sudo mv splitwise /usr/local/bin/splitwise
-echo "Build completed. Executable 'splitwise' created."
--- go.mod
+++
@@ -1,3 +0,0 @@
-module splitwise
-
-go 1.25.4
